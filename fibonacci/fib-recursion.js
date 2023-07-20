c5d59472c8c0be4e83a3fe860b320dd258b1d812
Janelle M <51424392+janellemeowers@users.noreply.github.com>
2023-07-19 20:15:41 -0400

    Update fib-recursion.js

--- fibonacci/fib-recursion.js
+++ fibonacci/fib-recursion.js
@@ -8,6 +8,7 @@ const fibsRec = (num) => {
 
   let array = fibsRec(num - 1);
 
+  //7, 6 etc until 2 returns [0,1] as starting array = [0,1] .push [1+0] and continue on
   array.push(array[array.length - 1] + array[array.length - 2]);
   return array;
 };
